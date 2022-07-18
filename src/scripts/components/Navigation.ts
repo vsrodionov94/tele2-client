aff234ed01fc720f603c1cd77864705be00f3fd8
Vladimir Rodionov <vsrodionov94@yandex.ru>
2022-07-18 21:06:21 +0700

    fix

--- src/scripts/components/Navigation.ts
+++ src/scripts/components/Navigation.ts
@@ -1,5 +1,5 @@
 import Game from './../scenes/Game';
-import { Screens } from '../types';
+import { Fonts, Screens } from '../types';
 import Utils from './../libs/Utils';
 
 export default class Navigation {
@@ -18,9 +18,19 @@ export default class Navigation {
     this.scene.add.sprite(centerX, height, 'nav-bg').setOrigin(0.5, 1).setDepth(1);
     const { currentScreen } = this.scene.state;
 
-    this.deferrerButton = this.scene.add.sprite(centerX - 220, height - 70, currentScreen !== Screens.Deferrer ? 'disable-deferrer' : 'active-deferrer').setDepth(1);
-    this.currentButton = this.scene.add.sprite(centerX, height - 70, currentScreen !== Screens.CurrentTask ? 'disable-current' : 'active-current').setDepth(1);
-    this.listButton = this.scene.add.sprite(centerX + 220, height - 70, currentScreen !== Screens.DoneList ? 'disable-list' : 'active-list').setDepth(1);
+    this.deferrerButton = this.scene.add.sprite(centerX - 220, height - 83, currentScreen !== Screens.Deferrer ? 'disable-deferrer' : 'active-deferrer').setDepth(1);
+    this.currentButton = this.scene.add.sprite(centerX, height - 83, currentScreen !== Screens.CurrentTask ? 'disable-current' : 'active-current').setDepth(1);
+    this.listButton = this.scene.add.sprite(centerX + 220, height - 83, currentScreen !== Screens.DoneList ? 'disable-list' : 'active-list').setDepth(1);
+
+    const fontStyle: Phaser.Types.GameObjects.Text.TextStyle = {
+      fontFamily: Fonts.Tele2DisplaySerif_Bold,
+      fontSize: '22px',
+      color: '#000000'
+    };
+
+    this.scene.add.text(this.deferrerButton.x, this.deferrerButton.y + 62, 'Отложенное', fontStyle).setOrigin(0.5).setDepth(1);
+    this.scene.add.text(this.currentButton.x, this.currentButton.y + 62, 'Сегодня', fontStyle).setOrigin(0.5).setDepth(1);
+    this.scene.add.text(this.listButton.x, this.listButton.y + 62, 'Выполненное', fontStyle).setOrigin(0.5).setDepth(1);
 
     if (currentScreen !== Screens.Deferrer) {
       Utils.clickButton(this.scene, this.deferrerButton, () => {
@@ -41,20 +51,6 @@ export default class Navigation {
         this.scene.state.currentScreen = Screens.DoneList;
         this.scene.scene.restart(this.scene.state);
       });
-    }
-  }
-
-  private updateState() {
-    switch(this.scene.state.currentScreen) {
-      case Screens.CurrentTask:
-        this.deferrerButton.setInteractive(true);
-        this.currentButton.setInteractive(false);
-        this.listButton.setInteractive(true);
-        break;
-      case Screens.Deferrer:
-        break;
-      case Screens.DoneList:
-        break;  
     }
   }
 };
